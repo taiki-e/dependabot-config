130a6eb28f5b491922f83a85c23509dc24f4b435
Taiki Endo <te316e89@gmail.com>
2021-04-17 03:47:30 +0900

    Clean up codegen

--- tools/codegen/src/main.rs
+++ tools/codegen/src/main.rs
@@ -7,6 +7,7 @@ use std::{
 
 use anyhow::Result;
 use quote::{format_ident, quote};
+use syn::visit_mut::{self, VisitMut};
 use walkdir::WalkDir;
 
 fn main() -> Result<()> {
@@ -81,11 +82,6 @@ fn gen_assert_impl() -> Result<()> {
     out += "// It is not intended for manual editing.\n";
     out += "\n";
 
-    let mut tokens = quote! {
-        use static_assertions::assert_impl_all as assert_impl;
-        use crate::*;
-    };
-
     let files: BTreeSet<String> = WalkDir::new(root_dir.join("src"))
         .into_iter()
         .filter_map(Result::ok)
@@ -94,40 +90,93 @@ fn gen_assert_impl() -> Result<()> {
             if !path.is_file() || path.extension() != Some("rs".as_ref()) {
                 return None;
             }
+            // Assertions are only needed for the library's public APIs.
+            if path.ends_with("main.rs") {
+                return None;
+            }
             Some(path.to_string_lossy().into_owned())
         })
         .collect();
+
+    let mut tokens = quote! {};
     for f in &files {
         let s = fs::read_to_string(f)?;
-        let ast = syn::parse_file(&s)?;
+        let mut ast = syn::parse_file(&s)?;
 
         let module = if f.ends_with("lib.rs") {
-            quote! {}
+            vec![]
         } else {
             let name = format_ident!("{}", Path::new(f).file_stem().unwrap().to_string_lossy());
-            quote! { #name:: }
+            vec![name.into()]
         };
 
-        for i in ast.items {
-            match i {
-                syn::Item::Struct(syn::ItemStruct { vis, ident, .. })
-                | syn::Item::Enum(syn::ItemEnum { vis, ident, .. })
-                    if matches!(vis, syn::Visibility::Public(..)) =>
-                {
-                    tokens.extend(quote! {
-                        assert_impl!(#module#ident: Send);
-                        assert_impl!(#module#ident: Sync);
-                        assert_impl!(#module#ident: Unpin);
-                    });
-                }
-                _ => {}
+        let mut visitor = ItemVisitor::new(module, |item, module| match item {
+            syn::Item::Struct(syn::ItemStruct { vis, ident, generics, .. })
+            | syn::Item::Enum(syn::ItemEnum { vis, ident, generics, .. })
+            | syn::Item::Union(syn::ItemUnion { vis, ident, generics, .. })
+            | syn::Item::Type(syn::ItemType { vis, ident, generics, .. })
+                if matches!(vis, syn::Visibility::Public(..)) =>
+            {
+                let lt_count = generics.lifetimes().count();
+                let lt = if lt_count > 0 {
+                    let lt = (0..lt_count).map(|_| quote! { '_ });
+                    quote! { <#(#lt)*> }
+                } else {
+                    quote! {}
+                };
+                tokens.extend(quote! {
+                    assert_send::<#(#module::)* #ident #lt>();
+                    assert_sync::<#(#module::)* #ident #lt>();
+                    assert_unpin::<#(#module::)* #ident #lt>();
+                });
             }
-        }
+            _ => {}
+        });
+        visitor.visit_file_mut(&mut ast);
     }
 
-    out += &tokens.to_string();
+    out += &quote! {
+        use crate::*;
+        const _: fn() = || {
+            fn assert_send<T: ?Sized + Send>() {}
+            fn assert_sync<T: ?Sized + Sync>() {}
+            fn assert_unpin<T: ?Sized + Unpin>() {}
+            #tokens
+        };
+    }
+    .to_string();
 
     fs::write(root_dir.join("src/gen/assert_impl.rs"), out)?;
 
     Ok(())
+}
+
+struct ItemVisitor<F> {
+    module: Vec<syn::PathSegment>,
+    f: F,
+}
+
+impl<F> ItemVisitor<F>
+where
+    F: FnMut(&mut syn::Item, &[syn::PathSegment]),
+{
+    fn new(module: Vec<syn::PathSegment>, f: F) -> Self {
+        Self { module, f }
+    }
+}
+
+impl<F> VisitMut for ItemVisitor<F>
+where
+    F: FnMut(&mut syn::Item, &[syn::PathSegment]),
+{
+    fn visit_item_mut(&mut self, item: &mut syn::Item) {
+        if let syn::Item::Mod(item) = item {
+            self.module.push(item.ident.clone().into());
+            visit_mut::visit_item_mod_mut(self, item);
+            self.module.pop();
+            return;
+        }
+        (self.f)(item, &self.module);
+        visit_mut::visit_item_mut(self, item);
+    }
 }
